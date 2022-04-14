918940b28c8b9899c7f77e3fe0ef74efd529137f
Lewis <ljoncb@gmail.com>
2022-04-14 16:21:49 +0100

    core reqs

--- src/server/controllers/movie.js
+++ src/server/controllers/movie.js
@@ -1,31 +1,58 @@
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = "mysecret";
 
 const getAllMovies = async (req, res) => {
-    const movies = await prisma.movie.findMany();
+  const movies = await prisma.movie.findMany();
 
-    res.json({ data: movies });
+  res.json({ data: movies });
+};
+
+const checkValidJWT = (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).json({ error: "unauthorized" });
+    return false;
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  if (!token) {
+    res.status(401).json({ error: "unauthorized" });
+    return false;
+  }
+
+  console.log("token", token);
+
+  try {
+    jwt.verify(token, jwtSecret);
+  } catch (err) {
+    console.error("error validating jwt", err);
+    res.status(401).json({ error: "unauthorized" });
+    return false;
+  }
+  return true;
 };
 
 const createMovie = async (req, res) => {
-    const { title, description, runtimeMins } = req.body;
+  const { title, description, runtimeMins } = req.body;
+  if (!checkValidJWT(req, res)) {
+    return;
+  }
 
-    try {
-        const token = null;
-        // todo verify the token
-    } catch (e) {
-        return res.status(401).json({ error: 'Invalid token provided.' })
-    }
+  const createdMovie = await prisma.movie.create({
+    data: {
+      title,
+      description,
+      runtimeMins,
+    },
+  });
 
-    const createdMovie = null;
-
-    res.json({ data: createdMovie });
+  res.json({ data: createdMovie });
 };
 
 module.exports = {
-    getAllMovies,
-    createMovie
+  getAllMovies,
+  createMovie,
 };
--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -5,30 +5,43 @@ const prisma = new PrismaClient();
 
 const jwtSecret = 'mysecret';
 
+const saltRounds = 10;
+
 const register = async (req, res) => {
     const { username, password } = req.body;
 
-    const createdUser = null;
+    const hashedPassword = bcrypt.hashSync(password, saltRounds)
 
+    const createdUser = await prisma.user.create({
+        data: {
+            username: username,
+            password: hashedPassword
+        }
+    });
+    delete createdUser.password
     res.json({ data: createdUser });
 };
 
 const login = async (req, res) => {
     const { username, password } = req.body;
 
-    const foundUser = null;
+    const foundUser = await prisma.user.findFirst({
+        where: {
+            username: username
+        }
+    });
 
     if (!foundUser) {
         return res.status(401).json({ error: 'Invalid username or password.' });
     }
 
-    const passwordsMatch = false;
+    const passwordsMatch = bcrypt.compareSync(password, foundUser.password);
 
     if (!passwordsMatch) {
         return res.status(401).json({ error: 'Invalid username or password.' });
     }
 
-    const token = null;
+    const token = jwt.sign({username: username}, jwtSecret);
 
     res.json({ data: token });
 };
